7c62a997fc8e3b41423a9ebb9920503c2eff309c
0xfederama <vgfede@gmail.com>
2020-06-05 11:23:23 +0200

    Add tray icon support

--- src/drink.go
+++ src/drink.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"os"
 	"path/filepath"
 	"runtime"
@@ -8,7 +10,23 @@ import (
 	"time"
 
 	"github.com/gen2brain/beeep"
+	"github.com/shurcooL/trayhost"
 )
+
+func notify(config, icon string) {
+	//Send first notification
+	message := "Start drinking now"
+	beeep.Alert("Drink!", message, icon)
+
+	delay := readDelay(config)
+
+	//While true send notifications sleeping every delay minutes
+	for {
+		time.Sleep(time.Duration(delay) * time.Minute)
+		message := "You haven't been drinking for " + strconv.Itoa(delay) + " minutes"
+		beeep.Alert("Drink!", message, icon)
+	}
+}
 
 func main() {
 
@@ -25,6 +43,7 @@ func main() {
 	configDirPath := filepath.Join(configPath, "water-reminder")
 	configFilePath := filepath.Join(configDirPath, "config.txt")
 	configIconPath := filepath.Join(configDirPath, "water-glass.png")
+	configIconPathBW := filepath.Join(configDirPath, "water-glass-bw.png")
 
 	if !findConfig(configPath) {
 		//Create config directory
@@ -33,20 +52,33 @@ func main() {
 		//Download icon and default config file in the new directory
 		downloadFile("https://raw.githubusercontent.com/0xfederama/water-reminder/master/resources/config.txt", configFilePath)
 		downloadFile("https://raw.githubusercontent.com/0xfederama/water-reminder/master/resources/water-glass.png", configIconPath)
+		downloadFile("https://raw.githubusercontent.com/0xfederama/water-reminder/master/resources/water-glass-bw.png", configIconPathBW)
 	}
 
-	//Send first notification after 5 seconds
 	time.Sleep(5 * time.Second)
-	message := "Start drinking now"
-	beeep.Alert("Drink!", message, configIconPath)
 
-	delay := readDelay(configFilePath)
+	menuItems := []trayhost.MenuItem{
+		{
+			Title: "Example Item",
+			Handler: func() {
+				fmt.Println("do stuff")
+			},
+		},
 
-	//While true send notifications sleeping every delay minutes
-	for {
-		time.Sleep(time.Duration(delay) * time.Minute)
-		message := "You haven't been drinking for " + strconv.Itoa(delay) + " minutes"
-		beeep.Alert("Drink!", message, configIconPath)
+		trayhost.SeparatorMenuItem(),
+		{
+			Title:   "Quit",
+			Handler: trayhost.Exit,
+		},
 	}
+
+	// Load tray icon.
+	iconData, _ := ioutil.ReadFile(configIconPathBW)
+
+	trayhost.Initialize("Water Reminder", iconData, menuItems)
+
+	go notify(configFilePath, configIconPath)
+
+	trayhost.EnterLoop()
 
 }
